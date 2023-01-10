c2e9b1b4078bc3682e782e94f87dd9a037b28663
Chris Zavala <frappejcat@gmail.com>
2023-01-10 16:35:43 -0600

    adding the first part of the js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,21 @@
-//Wrapping all my code in this so it interacts with the DOM in a call JQuery. ;)
-$(document).ready(function (){
+//Variable declaration: 
+var citysearchFormEl = document.querySelector("#city-search-form");
+var cityEl = document.querySelector("#city");
+var pastsearchBtn = document.querySelector("#past-search-buttons")
+var currentweatherContainerEl = document.querySelector("#current-weather-container");
+var fivedayForecastContainerEl = document.querySelector("#five-day-forecast");
+var foreCastEl = document.querySelector("#forecast");
+var searchedCityEl = document.querySelector("#searched-city");
+
+//Need a function to do the 'submit click' and do the search
+ var citySubmit = function(event) {
+    event.preventDefault();
+    console.log(event);
+
+    //local variable for my city search
+    // var city = cityEl.
+    
+ }
 
 
 
@@ -8,50 +24,4 @@ $(document).ready(function (){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-});
+citysearchFormEl.addEventListener("submit", citySubmit);
