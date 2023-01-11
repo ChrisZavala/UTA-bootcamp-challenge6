cfb0ac82f94f38110b720e15475bf7f3ee3ad47f
Chris Zavala <frappejcat@gmail.com>
2023-01-10 22:56:19 -0600

    add more to the js file and have the current weather working but the 5day forecast isn't

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,84 +1,158 @@
 //Variable declaration: 
-var citysearchFormEl = document.querySelector("#city-search-form");
-var cityEl = document.querySelector("#city");
-var pastsearchBtn = document.querySelector("#past-search-buttons")
-var currentweatherContainerEl = document.querySelector("#current-weather-container");
-var fivedayForecastContainerEl = document.querySelector("#five-day-forecast");
-var foreCastEl = document.querySelector("#forecast");
-var searchedCityEl = document.querySelector("#searched-city");
+var cityFormEl = document.querySelector("#city-search-form");
+var cityInputEl = document.querySelector("#city");
+var weatherContainerEl = document.querySelector("#current-weather-container");
+var citySearchInputEl = document.querySelector("#searched-city");
+var forecastTitle = document.querySelector("#forecast");
+var forecastContainerEl = document.querySelector("#five-day-forecast");
+var pastsearchBtn = document.querySelector("#past-search-buttons");
 //Array for my Cities list
-var CitiesArr = [];
+var cities = [];
+
+
+//Need a function to do the 'submit click' and do the search
+var citySubmitSearch = function(event) {
+    event.preventDefault();
+    var city = cityInputEl.value.trim();
+        if (city) {
+            currentCityWeather(city);
+            getFiveDayForeCast(city);
+            cities.unshift({city});
+            cityInputEl.value = "";
+        } else {
+            console.alert("Please Enter A City!");
+        }
+    //here I am calling my functions for past searches of cities and what is saved. 
+    savedCitySearch();
+    pastCitySearch(city);
+ }
+//This is my function that is storing my data locally to the webpage. 
+ var savedCitySearch = function() {
+    localStorage.setItem("cities", JSON.stringify(cities));
+ };
 
 var currentCityWeather = function(city) {
-    var apiKey = "a3831faa6502826b12c492add275b36e"  //my personal key from openweather.
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}"
+    var apiKey = "7d7c4de058f3c932d835b1c88eb5ec8c"
+    var apiURL =  `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${city}&units=imperial`
 
-    fetch{apiUrl}
-    .then(function(response) {
+    fetch(apiURL)
+    .then(function(response){
         response.json().then(function(data){
-            showWeather(city, data);
+            showcurrentCityWeather(data, city);
         });
     });
 };
 
+
 //this is going to be my function where I am getting my dt from api calls for city. 
-var showWeather = function(weather, searchCity) {
-    currentweatherContainerEl.textContent = "";
-    searchedCityEl.textContent = searchCity;
+var showcurrentCityWeather = function(weather, searchCity) {
+    weatherContainerEl.textContent= "";  
+    citySearchInputEl.textContent=searchCity;
 
     //going to create my city/date: Temp: Wind: Humidity: 
-    var currentDate = document.createElement("span");
-    currentDate.textContent = " (" moment(weather.dt.value).format("MMM D, YYYY") + ") ";
-    searchedCityEl.appendChild(currentDate);
+    var currentDate = document.createElement("span")
+    currentDate.textContent=" (" + moment(weather.dt.value).format("MMM D, YYYY") + ") ";
+    citySearchInputEl.appendChild(currentDate);
 
-    var Icon = document.createElement("img");
-    Icon.setAttribute("src", "https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png");
-    searchedCityEl.appendChild(Icon);
+    var weatherIcon = document.createElement("img")
+   weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
+   citySearchInputEl.appendChild(weatherIcon);
 
-    var currentCityTemperature = document.createElement("span");
-    currentCityTemperature.textContent = "Temp: " + weather.main.temp + " °F";
-    currentCityTemperature.classList = "list-group-item" 
+   var temperatureEl = document.createElement("span");
+   temperatureEl.textContent = "Temperature: " + weather.main.temp + " °F";
+   temperatureEl.classList = "list-group-item"
 
-    var currentCityWind = document.createElement("span");
-    currentCityWind.textContent = "Wind: " + weather.wind.speed + " MPH";
-    currentCityWeather.classList = "list-group-item" 
+   var windSpeedEl = document.createElement("span");
+   windSpeedEl.textContent = "Wind Speed: " + weather.wind.speed + " MPH";
+   windSpeedEl.classList = "list-group-item"
 
-    var currentCityHumid = document.createElement("span");
-    currentCityHumid.textContent = "Humidity: " + weather.main.humidity + " %";
-    currentCityHumid.classList = "list-group-item" 
+    var humidityEl = document.createElement("span");
+    humidityEl.textContent = "Humidity: " + weather.main.humidity + " %";
+    humidityEl.classList = "list-group-item"
 
     //now I need to append the child element again to put in the weather container.. 
-    currentweatherContainerEl.appendChild(currentCityTemperature);
-    currentweatherContainerEl.appendChild(currentCityWind);
-    currentweatherContainerEl.appendChild(currentCityHumid);
+    weatherContainerEl.appendChild(temperatureEl);
+    weatherContainerEl.appendChild(windSpeedEl);
+    weatherContainerEl.appendChild(humidityEl);
 
 
 }
 
 
+var getFiveDayForeCast = function(city){
+    var apiKey = "7d7c4de058f3c932d835b1c88eb5ec8c"
+    var apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`
 
+    fetch(apiURL)
+    .then(function(response){
+        response.json().then(function(data){
+            getFiveDayForeCast(data);
+        });
+    });
+};
 
-//Need a function to do the 'submit click' and do the search
- var citySubmit = function(event) {
-    event.preventDefault();
-    console.log(event);
+var showfiveDayForeCast = function () {
+    forecastContainerEl.textContent = "";
+    forecastTitle.textContent = "5-Day Forecast:";
 
-    //local variable for my city search
-    //need my function for current city weather
-    //need my function for the 5-day forecast
-    //need my funciton for savedSearch 
-    //need my function for the past searchedCity
+    var forecast = weather.list;
 
-    var city = cityEl.value.trim();
-        if (city) {
+        for (var i = 5; i < forecast.length; i = i+8) {
+        var dailyForecast = forecast[i];
+        
+        var forecastEl = document.createElement("div");
+        forecastEl.classList = "card bg-primary text-light m-2";
 
+        var forecastDate = document.createElement("h5");
+        forecastDate.textContent = moment.unix(dailyForecast.dt).format("MMM D, YYYY");
+        forecastDate.classList = "card-header text-center";
+        forecastEl.appendChild.apply(forecastDate);
 
+        var weatherIcon = document.createElement("img")
+        weatherIcon.classList = "card-body text-center";
+        weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${dailyForecast.weather[0].icon}@2x.png`);
 
-        }
-    
+        var forecastTemperature = document.createElement("span");
+        forecastTemperature.classList = "card-body text-center";
+        forecastTemperature.textContent = dailyForecast.main.temp + " °F";
+        
+
+        // var foreCastWind = document.createElement("span");
+        // foreCastWind.classList = "card-body text-center";
+        // foreCastWind.textContent =   dailyForecast.main.wind.speed + " MPH"
+       
+
+        var forecastHumidity = document.createElement("span");
+        forecastHumidity.classList = "card-body text-center";
+        forecastHumidity.textContent = dailyForecast.main.humidity + " %"
+        
+        forecastEl.appendChild(weatherIcon);
+        dailyForeCast.appendChild(forecastTemperature);
+        // dailyForeCast.appendChild(foreCastWind);
+        dailyForecast.appendChild(forecastHumidity);
+    }
+
+}
+
+ //creating my buttons from the past searches that are on the list. 
+ var pastCitySearch = function (pastSearch) {
+    pastSearchEl = document.createElement("button");
+    pastSearchEl.textContent = pastSearch;
+    pastSearchEl.classList = "d-flex w-100 btn-light border p-2";
+    pastSearchEl.setAttribute("data-city", pastSearch);
+    pastSearchEl.setAttribute("type", "submit");
+    pastsearchBtn.prepend(pastSearchEl);
+
  }
 
+ var pastCitySearchHistory = function(event) {
+    var city = event.target.getAttribute("data-city");
+    console.log(event);
+    if (city) {
+        currentCityWeather(city);
+        getFiveDayForeCast(city);
+    }
+ }
 
-
-
-citysearchFormEl.addEventListener("submit", citySubmit);
+cityFormEl.addEventListener("submit", citySubmitSearch);
+pastsearchBtn.addEventListener("submit", pastCitySearchHistory);
